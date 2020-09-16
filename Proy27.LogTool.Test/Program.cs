2cfa0baaaa55b2c7ae1ba62757fd124963c0cc76
proy27 <proy55663000@gmail.com>
2020-09-17 00:53:47 +0800

    0

---
+++ Proy27.LogTool.Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Proy27.LogTool.Test
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			var sw = Stopwatch.StartNew();
+			Thread.Sleep(888);
+			Console.WriteLine(sw.Avg(3));
+		}
+	}
+}
---
+++ Proy27.LogTool/Extend.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Proy27.LogTool
+{
+	public static class Extend
+	{
+		public static string To4(this double a)
+		{
+			return a.ToString("0.0000");
+		}
+		public static string To1(this double a)
+		{
+			return a.ToString("0.0");
+		}
+		public static string To4(this Stopwatch a)
+		{
+			return a.Elapsed.TotalSeconds.To4();
+		}
+		public static double TS(this Stopwatch a)
+		{
+			return a.Elapsed.TotalSeconds;
+		}
+		public static string Avg(this Stopwatch a, long count)
+		{
+			return (count / a.Elapsed.TotalSeconds).To4();
+		}
+	}
+}
